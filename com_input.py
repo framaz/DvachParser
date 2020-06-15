ee6115b54948d7bdc9fe1000820b13ae71357aea
framaz <framaz@yandex.ru>
2020-06-15 17:37:38 +0500

    feat: proper output of command results.

--- com_input.py
+++ com_input.py
@@ -15,6 +15,14 @@ class DvachInput(ConsoleDebugInput):
                 path = data[2]
             return DownloaderFromDvach(data[1], path)
 
+    async def output_command_result(self, command_result):
+        if isinstance(command_result, list):
+            print("Listing all active downloads:")
+            for i, provider in enumerate(command_result):
+                print(f"{i} - {provider}")
+            return
+        print(command_result)
+
 
 class DownloaderFromDvach(Command):
     def __init__(self, link: str, save_path: str = None, source=None):
@@ -35,4 +43,4 @@ class DownloaderFromDvach(Command):
         if found == -1:
             NewProviderCommand("dv", args=[self._link]).execute_command(phfsys)
         NewHookCommand("dvD", found, self._save_path).execute_command(phfsys)
-        return "Success"
+        return f"Downloading from {self._link} started."
